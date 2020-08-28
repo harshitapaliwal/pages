1803ba30a7ca58e9a89bca14be6e24300b903545
DJ <deepakj6@yahoo.co.in>
2020-08-28 17:42:54 +0100

    DB Tier Start

---
+++ database/migrations/V1__inital_schema.sql
@@ -0,0 +1,11 @@
+USE pages;
+create table pages(
+id bigint(20) not null auto_increment,
+business_name VARCHAR(50),
+address VARCHAR(50),
+category_id bigint(20),
+contact_number VARCHAR(50),
+primary key (id)
+)
+engine = innodb
+default charset = utf8;
