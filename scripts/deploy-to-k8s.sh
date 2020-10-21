18943ff84e6cb3972f2c85c1e7d25a8c4adbb2f7
harshita <harshitapaliwal23@gmail.com>
2020-10-21 08:06:00 +0100

    lab9 persistentvolume

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -6,8 +6,12 @@ kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
 kubectl apply -f deployment/pages-deployment.yaml
 
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
+
 kubectl apply -f deployment/mysql-secret.yaml
 kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/flyway-configmap.yaml
 kubectl apply -f deployment/flyway-job.yaml
+
